95f2d5136c55ddb485e0fa64566d2df8bbaf05a1
EmmaGhitau <ghitau.emma@yahoo.com>
2022-11-13 22:54:14 +0200

    Add project files.

---
+++ Data/Ghitau_Emanuela_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ghitau_Emanuela_Lab2.Models;
+
+namespace Ghitau_Emanuela_Lab2.Data
+{
+    public class Ghitau_Emanuela_Lab2Context : DbContext
+    {
+        public Ghitau_Emanuela_Lab2Context (DbContextOptions<Ghitau_Emanuela_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ghitau_Emanuela_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
